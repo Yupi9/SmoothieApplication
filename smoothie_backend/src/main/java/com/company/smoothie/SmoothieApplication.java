a26556ad22a0c4d5a60910651ab9c9af04b2ba4b
Aliaksandr <lepeshko1997@gmail.com>
2023-02-22 14:35:00 +0100

    edit cors policy

--- smoothie_backend/src/main/java/com/company/smoothie/SmoothieApplication.java
+++ smoothie_backend/src/main/java/com/company/smoothie/SmoothieApplication.java
@@ -2,16 +2,30 @@ package com.company.smoothie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaRepositories
 @EnableWebMvc
 public class SmoothieApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmoothieApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SmoothieApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
 
 }
