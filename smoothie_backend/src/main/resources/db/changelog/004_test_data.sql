759d80770dec95138235b0f6ff51330089d95f0f
Aliaksandr <lepeshko1997@gmail.com>
2023-02-23 18:40:04 +0100

    added initial data

---
+++ smoothie_backend/src/main/resources/db/changelog/004_test_data.sql
@@ -0,0 +1,34 @@
+INSERT INTO nutrition(energy, protein, fat, carbohydrate)
+VALUES(391, 15, 12, 50);
+INSERT INTO public.smoothie("name", ingredients, nutrition_id)
+VALUES('Breakfast super-shake', 'banana, blueberries, cinnamon', 1);
+
+INSERT INTO nutrition(energy, protein, fat, carbohydrate)
+VALUES(156, 4, 3, 25);
+INSERT INTO public.smoothie("name", ingredients, nutrition_id)
+VALUES('Two-minute breakfast smoothie', 'banana,  porridge oats, milk, honey', 2);
+
+INSERT INTO nutrition(energy, protein, fat, carbohydrate)
+VALUES(170, 3, 1, 34);
+INSERT INTO public.smoothie("name", ingredients, nutrition_id)
+VALUES('Strawberry smoothie', 'strawberries, banana, orange juice', 3);
+
+INSERT INTO nutrition(energy, protein, fat, carbohydrate)
+VALUES(230, 9, 3, 37);
+INSERT INTO public.smoothie("name", ingredients, nutrition_id)
+VALUES('Smoothie bowl', 'frozen mixed berries, banana, oat milk, maple syrup', 4);
+
+INSERT INTO nutrition(energy, protein, fat, carbohydrate)
+VALUES(226, 8, 13, 16);
+INSERT INTO public.smoothie("name", ingredients, nutrition_id)
+VALUES('Strawberry green goddess smoothie', 'ripe strawberries, baby spinach, small avocado, small oranges', 5);
+
+INSERT INTO nutrition(energy, protein, fat, carbohydrate)
+VALUES(106, 4, 3, 14);
+INSERT INTO public.smoothie("name", ingredients, nutrition_id)
+VALUES('Raspberry and apple smoothie', 'apples, raspberries, yogurt', 6);
+
+INSERT INTO nutrition(energy, protein, fat, carbohydrate)
+VALUES(176, 7, 11, 11);
+INSERT INTO public.smoothie("name", ingredients, nutrition_id)
+VALUES('Green spirulina smoothie', 'avocado, spirulina powder, baby spinach, mint', 7);
